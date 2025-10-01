236c0d3c447c175910a6d612955d0be5a78dc70c
The Tinkerer <tostinkerer@gmail.com>
2025-10-01 09:34:34 -0500

    add warnings when image is exported wrong or dimensions are wrong

--- raw2bytearray.py
+++ raw2bytearray.py
@@ -7,6 +7,14 @@ with open(sys.argv[1], "rb") as f:
 width = int(sys.argv[2])
 height = int(sys.argv[3])
 
+if len(data) == 2*width*height:
+    print("Your image is double the size it should be!  Remove the alpha channel and re-export it!")
+    sys.exit(1)
+
+if len(data) != width*height:
+    print(f"Your image dimensions are incorrect! {sys.argv[1]} is not exactly {width} x {height} = {width*height} bytes!  Make sure you exported it as RAW!")
+    sys.exit(1)
+
 print("static U8 image[{}][{}] = {{".format(height, width))
 for y in range(height):
     row = data[y*width:(y+1)*width]
